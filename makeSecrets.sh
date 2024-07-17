adbcd833ac02cc3737293382239e76c5e7c346ce
nicodeclercq <nicolas.declercq@doctolib.com>
2024-07-17 18:20:55 +0200

    fix

--- makeSecrets.sh
+++ makeSecrets.sh
@@ -1,11 +1,7 @@
 cat > ./src/secrets.ts << EOF
 export const secrets = {
   salt: '$SALT',
-  firebaseApiKey: '$FIREBASE_API_KEY',
-  firebaseAuthDomain: '$FIREBASE_AUTH_DOMAIN',
-  firebaseProjectId: '$FIREBASE_PROJECT_ID',
-  firebaseStorageBucket: '$FIREBASE_STORAGE_BUCKET',
-  firebaseMessagingSenderId: '$FIREBASE_MESSAGING_SENDER_ID',
-  firebaseAppId: '$FIREBASE_APP_ID',
+  supabaseUrl: '$SUPABASE_URL',
+  supabaseKey: '$SUPABASE_KEY',
 };
 EOF
