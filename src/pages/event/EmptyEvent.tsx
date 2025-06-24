47a787a78a0f2e9ff9972b2038927aa475355c89
nicodeclercq <nicolas.declercq@doctolib.com>
2025-06-24 13:05:01 +0200

    fix empty state

--- src/pages/event/EmptyEvent.tsx
+++ src/pages/event/EmptyEvent.tsx
@@ -12,14 +12,22 @@ export function EmptyEvent({ event }: Props) {
   return (
     <EmptyState
       title={t("page.event.empty.title")}
-      description={t("page.event.empty.description")}
+      description={t(
+        event.isClosed
+          ? "page.event.empty.description.closed"
+          : "page.event.empty.description"
+      )}
       illustration="card-stack"
-      action={{
-        as: "link",
-        label: t("page.event.empty.actions.add"),
-        to: "EXPENSE_ADD",
-        params: { eventId: event._id },
-      }}
+      action={
+        event.isClosed
+          ? undefined
+          : {
+              as: "link",
+              label: t("page.event.empty.actions.add"),
+              to: "EXPENSE_ADD",
+              params: { eventId: event._id },
+            }
+      }
     />
   );
 }
