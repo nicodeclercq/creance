af93e99e64ca5d3e21fc5967638a857e3e9b7e11
nicodeclercq <nicolas.declercq@doctolib.com>
2025-06-26 13:16:37 +0200

    try put sync back

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import "./App.css";
 
 import {
+  $store,
   StoreProvider,
   load,
   onChange,
+  update,
 } from "./store/StoreProvider";
 
 import { Debug } from "./ui/Debug/Debug";
 import { I18nProvider } from "react-aria";
 import { Router } from "./router";
-// import { synchronizeFirebase } from "./service/firebase";
+import { synchronizeFirebase } from "./service/firebase";
 import { synchronizeLocalStorage } from "./store/localStorage";
 
 // Synchronize local storage with the store
 synchronizeLocalStorage({ load, onChange });
-// synchronizeFirebase({ $store, update });
+synchronizeFirebase({ $store, update });
 
 function App() {
   return (
