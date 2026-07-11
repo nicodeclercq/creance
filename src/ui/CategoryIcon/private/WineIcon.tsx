7b9a53c12cada90c70b500e4f70bc2a14687c608
nicodeclercq <nicolas.declercq@doctolib.com>
2026-07-11 11:24:03 +0200

    fix

--- src/ui/CategoryIcon/private/WineIcon.tsx
+++ src/ui/CategoryIcon/private/WineIcon.tsx
@@ -5,6 +5,7 @@ export const WineIcon = () => (
       fillRule="evenodd"
       d="M3 20.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5"
       clipRule="evenodd"
+      aria-hidden="true"
     />
     <path
       fill="currentColor"
