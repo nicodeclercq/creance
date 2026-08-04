003cea758338ffe4e2669d6aab422b2009d644c7
nicodeclercq <nicolas.declercq@doctolib.com>
2026-08-04 09:34:47 +0200

    fix

--- src/pages/join/private/Signup.tsx
+++ src/pages/join/private/Signup.tsx
@@ -1,7 +1,9 @@
+import type { FirebaseError } from "firebase/app";
 import { Heading } from "../../../ui/Heading/Heading";
-import { LoginForm } from "../../auth/private/LoginForm";
+import { SignupForm } from "../../auth/private/SignupForm";
 import { Stack } from "../../../ui/Stack/Stack";
 import { useAuth } from "../../../store/useAuth";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 type SignupProps = {
@@ -12,12 +14,23 @@ type SignupProps = {
 export function Signup({ onNext, switchToLogin }: SignupProps) {
   const { t } = useTranslation();
   const { signup } = useAuth();
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(
+    undefined,
+  );
 
   return (
     <Stack gap="m">
       <Heading>{t("SignupForm.title")}</Heading>
-      <LoginForm
-        onSubmit={(credentials) => signup(credentials).then(onNext)}
+      <SignupForm
+        errorMessage={errorMessage}
+        onSubmit={(credentials) =>
+          signup(credentials)
+            .then(onNext)
+            .catch((error: FirebaseError) => {
+              console.log(error);
+              setErrorMessage(t(`Signup.error.message.${error.code}`));
+            })
+        }
         cancel={{
           label: t("SignupForm.actions.login"),
           onClick: switchToLogin,
